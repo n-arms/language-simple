ac5619341e53298bae2a99e6649b4619b6de999b
narms <75282394+n-arms@users.noreply.github.com>
2021-02-03 11:22:57 -0500

    initial commit

---
+++ spec/language-simple-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageSimpleView from '../lib/language-simple-view';
+
+describe('LanguageSimpleView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
